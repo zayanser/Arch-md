9c075ec89c16be60caa3792b0309c58a72ca7723
Ednut <147618496+Ednut001@users.noreply.github.com>
2024-12-29 10:33:50 +0100

    Arch-md²⁴

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.typeMenu = 'v5' //don't change
 global.anticall = false // set true to turn anticall always on
 global.xprefix = '.' // your desired prefix
 global.presence = 'available' // available to show always online // unavailable to show not oniline //composing to show fake typing //recording to show fake recording audio
-global.thumb = 'https://pomf2.lain.la/f/4pdqc01o.jpg' // your menu image
+global.thumb = 'https://files.catbox.moe/9nrqqg.jpg' // your menu image
 global.timezone = 'Africa/Lagos' // input timezone codrectly with continuent then / city if input wrongly might cause error
 //======= Don't change =======\\
 
