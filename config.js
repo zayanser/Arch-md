b32e8807573bb761775c3ebcab94cdde78aa0727
Ednut <147618496+Ednut001@users.noreply.github.com>
2024-12-29 11:32:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"] 
+global.owner = ["2347036214381"]
 global.botname = ["Arch Md"]
 global.simbol = "♘"
 global.typeMenu = 'v5' //don't change 
