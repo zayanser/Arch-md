1a57c32f13fca530d349a17fb9bde1e8de6098cb
Ednut <147618496+Ednut001@users.noreply.github.com>
2025-02-03 23:10:12 +0100

    © ᴀʀᴄʜ ᴍᴅ ²⁵

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
-const {fork} = require('child_process');
-const chalk = require('chalk')
+const path = require('path');
+const { spawn } = require('child_process');
 
-async function start(){
-const child = fork('./index.js')
-//send message to child
-//child.send("Hello Child")
-
-//send child to parent
-child.on("message",msg=>{
-console.log('child to parent =>',msg)
-})
-
-child.on("close",(anu)=>{
-//console.log('terclose',anu)
-console.log(chalk.black(chalk.bgRed(`Arch Md is restarting..`)))
-start()
-})
-
-child.on("exit",(anu2)=>{
-//console.log('terexit',anu2)
-})
-
+function start() {
+	let args = [path.join(__dirname, 'index.js'), ...process.argv.slice(2)]
+	let p = spawn(process.argv[0], args, {
+		stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+	}).on('message', data => {
+		if (data == 'reset') {
+			console.log('Restarting Bot...')
+			p.kill()
+			start()
+			delete p
+		}
+	}).on('exit', code => {
+		console.error('Exited with code:', code)
+		if (code == '.' || code == 1 || code == 0) start()
+	})
 }
 start()
