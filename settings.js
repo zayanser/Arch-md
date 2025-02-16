70a4de541d86ec0a90ce37c98ea191967782bbdf
Ednut <147618496+Ednut001@users.noreply.github.com>
2025-02-16 18:59:23 +0100

    © ᴀʀᴄʜ ᴍᴅ ²⁵

--- settings.js
+++ settings.js
@@ -9,23 +9,25 @@
 const fs = require('fs');
 const chalk = require('chalk');
 const { version } = require("./package.json")
-
+require('dotenv').config(); 
 
 //======= Change settings =======\\
-global.owner = "2347036214381"
-global.botname = "αrch md"
-global.ownername = "" //set bot owner name here 
-global.simbol = "♘"
-global.typeMenu = 'v2' // choose between 1,2,3
-global.prefix = '.' // your desired prefix symbol only
-global.channel = ''
-global.timezone = 'Africa/Lagos'
-global.author = 'Ednut'
-global.packname = 'Arch Md ²⁵'
+global.owner = process.env.OWNER_NUMBER || "2347036214381"
+global.botname = process.env.BOT_NAME || "αrch md"
+global.ownername = process.env.OWNER_NAME || "Ednut"; //set bot owner name here 
+global.prefix = process.env.PREFIX || "."; // your desired prefix symbol only
+global.timezone = process.env.TIME_ZONE || "Africa/Lagos";
+global.author = process.env.AUTHOR  || "Ednut";
+global.packname = process.env.PACK_NAME  || "Arch Md ²⁵";
+global.startup = process.env.STARTUP_MSG === 'true';
+global.typeMenu = process.env.MENU_TYPE  || "v2";
+global.onlypc = process.env.ONLYPC_MSG  || "Sorry buddy the bot won't work directly in group chat to reduce spam use in private chat";
+global.onlygroup = process.env.ONLYGC_MSG  || "Sorry buddy the bot won't work directly in private chat to reduce spam use in group";
 
+
+global.simbol = "♘"
 global.tempatDB = 'database.json' // dont change might cause big errors 
 global.pairing_code = false // if deploying on panel set true if on any other platform leave it false
-global.startup = true
 
 // Settings Image Url
 global.image = {
