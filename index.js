10d18b67935efc56b110dca82fea7b46117e6a32
Ednut <147618496+Ednut001@users.noreply.github.com>
2025-05-20 13:16:39 +0100

    © ᴀʀᴄʜ ᴍᴅ ²⁵
    Added render deployment

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const {fork} = require('child_process');
-const chalk = require('chalk')
+const { fork } = require('child_process');
+const chalk = require('chalk');
+const express = require('express');
+const app = express();
+const __path = process.cwd();
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
 
-async function start(){
-const child = fork('./main.js')
-//send msg to child
-//child.send("Hello Child")
+async function start() {
+  const child = fork('./main.js');
 
-//check child response to child
-child.on("message",msg=>{
-console.log('child to parent =>',msg)
-})
+  child.on("message", msg => {
+    console.log('child to parent =>', msg);
+  });
 
-child.on("close",(anu)=>{
-//console.log('terclose',anu)
-console.log(chalk.black(chalk.bgRed(`Arch Wa Ai is restarting..`)))
-start()
-})
+  child.on("close", () => {
+    console.log(chalk.black(chalk.bgRed(`Arch md is restarting..`)));
+    start();
+  });
 
-child.on("exit",(anu2)=>{
-//console.log('terexit',anu2)
-})
+  child.on("exit", () => {});
 
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:` + PORT);
+  });
 }
-start()
+
+start();
